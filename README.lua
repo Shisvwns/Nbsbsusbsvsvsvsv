abe1ebb99fed8068f4be2eda8bdaf45d14c770df
Shisvwns <165137013+Shisvwns@users.noreply.github.com>
2024-03-27 15:50:27 +0700

    Update README.lua

--- README.lua
+++ README.lua
@@ -5417,6 +5417,8 @@ end)
 ----------------------------------------------------------------------------------------------------------------------------------------------
 -- [ Tab Sea Event ]
 
+local Section = Tabs.Sea:AddSection("Sea Event")
+
 AllBoat = {
 	"Dinghy",
 	"Pirate Sloop",
@@ -5437,8 +5439,6 @@ Boat:OnChanged(function(Value)
     SelectBoat = Value
 end)
 
-local Section = Tabs.Sea:AddSection("Sea Event")
-
 local BuyBoat = Tabs.Sea:AddToggle("BuyBoat1", {Title = "Auto Sail Boat ", Default = false })
 Options.BuyBoat1:SetValue(false)
 BuyBoat:OnChanged(function(Value)
@@ -5451,13 +5451,13 @@ spawn(function()
         pcall(function()
             if _G.SailBoat then
                 if not game:GetService("Workspace").Enemies:FindFirstChild("Shark") or not game:GetService("Workspace").Enemies:FindFirstChild("Terrorshark") or not game:GetService("Workspace").Enemies:FindFirstChild("Piranha") or not game:GetService("Workspace").Enemies:FindFirstChild("Fish Crew Member") then
-                    if not game:GetService("Workspace").Boats:FindFirstChild("PirateBrigade") then
+                    if not game:GetService("Workspace").Boats:FindFirstChild(SelectBoat) then
                         buyb = TweenBoat(CFrame.new(-16927.451171875, 9.0863618850708, 433.8642883300781))
                         if (CFrame.new(-16927.451171875, 9.0863618850708, 433.8642883300781).Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).magnitude <= 10 then
                             if buyb then buyb:Stop() end
                             local args = {
                                 [1] = "BuyBoat",
-                                [2] = "PirateBrigade"
+                                [2] = SelectBoat
                             }
                             game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
                         end
@@ -5466,7 +5466,7 @@ spawn(function()
                             TweenBoat(game:GetService("Workspace").Boats.PirateGrandBrigade.VehicleSeat.CFrame * CFrame.new(0,1,0))
                         else
                             for i,v in pairs(game:GetService("Workspace").Boats:GetChildren()) do
-                                if v.Name == "PirateBrigade" then
+                                if v.Name == SelectBoat then
                                     repeat wait(_G.FastAttackDelay)
                                         if (CFrame.new(-17013.80078125, 10.962434768676758, 438.0169982910156).Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).magnitude <= 10 then
                                             TweenShip(CFrame.new(-33163.1875, 10.964323997497559, -324.4842224121094))
