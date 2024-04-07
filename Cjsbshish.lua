5ad7a29df38fae4afc6a1cf45a2ed99d0baf234f
Shisvwns <165137013+Shisvwns@users.noreply.github.com>
2024-04-07 16:09:06 +0700

    Update Cjsbshish.lua

--- Cjsbshish.lua
+++ Cjsbshish.lua
@@ -7245,26 +7245,6 @@ spawn(function()
     end
 end)
 
-local BuyG = Tabs.Race:AddToggle("BuyG1", {Title = "Auto Buy Gear", Default = false })
-BuyG:OnChanged(function(Value)
-    _G.BuyGear = Value
-    StopTween(_G.BuyGear)
-end)
-
-spawn(function()
-    pcall(function()
-        while wait(0.1) do
-            if _G.BuyGear then
-                local args = {
-                    [1] = "UpgradeRace",
-                    [2] = "Buy"
-                }
-                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer(unpack(args))
-            end
-        end 
-    end)  
-end)
-
 -- [ Tab Stats & Esp ]
 
 local Section = Tabs.StatsEsp:AddSection("Stats")
