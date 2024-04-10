534eba43dbd7389e7c4e6f508b1567cbc91397df
Shisvwns <165137013+Shisvwns@users.noreply.github.com>
2024-04-10 10:23:09 +0700

    Update Cjsbshish.lua

--- Cjsbshish.lua
+++ Cjsbshish.lua
@@ -2851,79 +2851,6 @@ spawn(function()
     end)
 end)
 
-function MoonTextureId()
-    if World1 then
-        return game:GetService("Lighting").FantasySky.MoonTextureId
-    elseif World2 then
-        return game:GetService("Lighting").FantasySky.MoonTextureId
-    elseif World3 then
-        return game:GetService("Lighting").Sky.MoonTextureId
-    end
-end
-function CheckMoon()
-    moon8 = "http://www.roblox.com/asset/?id=9709150401"
-    moon7 = "http://www.roblox.com/asset/?id=9709150086"
-    moon6 = "http://www.roblox.com/asset/?id=9709149680"
-    moon5 = "http://www.roblox.com/asset/?id=9709149431"
-    moon4 = "http://www.roblox.com/asset/?id=9709149052"
-    moon3 = "http://www.roblox.com/asset/?id=9709143733"
-    moon2 = "http://www.roblox.com/asset/?id=9709139597"
-    moon1 = "http://www.roblox.com/asset/?id=9709135895"
-    moonreal = MoonTextureId()
-    cofullmoonkothangbeo = "Bad Moon"
-    if moonreal == moon5 or moonreal == moon4 then
-        if moonreal == moon5 then
-            cofullmoonkothangbeo = "Full Moon"
-        elseif moonreal == moon4 then
-            cofullmoonkothangbeo = "Next Night"
-        end
-    end
-    return cofullmoonkothangbeo
-end
-function function7()
-    GameTime = "Error"
-    local c = game.Lighting
-    local ao = c.ClockTime
-    if ao >= 18 or ao < 5 then
-        GameTime = "Night"
-    else
-        GameTime = "Day"
-    end
-    return GameTime
-end
-function function6()
-    return math.floor(game.Lighting.ClockTime)
-end
-function getServerTime()
-    RealTime = tostring(math.floor(game.Lighting.ClockTime * 100) / 100)
-    RealTime = tostring(game.Lighting.ClockTime)
-    RealTimeTable = RealTime:split(".")
-    Minute, Second = RealTimeTable[1], tonumber(0 + tonumber(RealTimeTable[2] / 100)) * 60
-    return Minute, Second
-end
-function function8()
-    local c = game.Lighting
-    local ao = c.ClockTime
-    if CheckMoon() == "Full Moon" and ao <= 5 then
-        return tostring(function6()) .. " ( Will End Moon In " .. math.floor(5 - ao) .. " Minutes )"
-    elseif CheckMoon() == "Full Moon" and (ao > 5 and ao < 12) then
-        return tostring(function6()) .. " ( Fake Moon )"
-    elseif CheckMoon() == "Full Moon" and (ao > 12 and ao < 18) then
-        return tostring(function6()) .. " ( Will Full Moon In " .. math.floor(18 - ao) .. " Minutes )"
-    elseif CheckMoon() == "Full Moon" and (ao > 18 and ao <= 24) then
-        return tostring(function6()) .. " ( Will End Moon In " .. math.floor(24 + 6 - ao) .. " Minutes )"
-    end
-    if CheckMoon() == "Next Night" and ao < 12 then
-        return tostring(function6()) .. " ( Will Full Moon In " .. math.floor(18 - ao) .. " Minutes )"
-    elseif CheckMoon() == "Next Night" and ao > 12 then
-        return tostring(function6()) .. " ( Will Full Moon In " .. math.floor(18 + 12 - ao) .. " Minutes )"
-    end
-    return tostring(function6())
-end
-function FullMoobCheck()
- return function8()
-end
-
 spawn(function()
     while wait() do
         if sethiddenproperty then
@@ -2963,7 +2890,7 @@ local Section = Tabs.Farm:AddSection("Setting Farm")
 
 local SelectWeaponFarm = Tabs.Farm:AddDropdown("SelectWeaponFarm", {
 	Title = "Select Weapon",
-	Values = {"Melee","Sword","Gun","Devil Fruit"},
+	Values = {"Melee","Sword","Gun","Blox Fruit"},
 	Multi = false,
 	Default = 1,
 })
@@ -2998,7 +2925,7 @@ spawn(function()
 						end
 					end
 				end
-			elseif _G.SelectWeapon == "Devil Fruit" then
+			elseif _G.SelectWeapon == "Blox Fruit" then
 				for i ,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
 					if v.ToolTip == "Blox Fruit" then
 						if game.Players.LocalPlayer.Backpack:FindFirstChild(tostring(v.Name)) then
@@ -6911,34 +6838,6 @@ Tabs.Player:AddButton({
 
 local Section = Tabs.Race:AddSection("Templete Of Time")
 
-function CheckRace()
-local a = game.ReplicatedStorage.Remotes.CommF_:InvokeServer("Wenlocktoad","1")
-local b = game.ReplicatedStorage.Remotes.CommF_:InvokeServer("Alchemist","1")
-if game.Players.LocalPlayer.Character:FindFirstChild("RaceTransformed") then
-return game:GetService("Players").LocalPlayer.Data.Race.Value.." V4"
-end
-if a == -2 then
-return game:GetService("Players").LocalPlayer.Data.Race.Value.." V3"
-end
-if b == -2 then
-return game:GetService("Players").LocalPlayer.Data.Race.Value.." V2"
-end
-return game:GetService("Players").LocalPlayer.Data.Race.Value.." V1"
-end
-
-local CRace = Tabs.Race:AddParagraph({
-    Title = "Your Race",
-    Content = "..."
-})
-
-spawn(function()
-    while wait() do
-        pcall(function()
-            CRace:SetDesc("Race: "..CheckRace())
-        end)
-    end
-end)
-
 Tabs.Race:AddButton({
     Title = "Teleport To Temple Of Time",
     Description = "",
@@ -7131,15 +7030,34 @@ end)
 
 local Section = Tabs.Race:AddSection("Trials")
 
-local Moon1 = Tabs.Race:AddParagraph({
-    Title = "Full Moon Status",
+function CheckRace()
+local a = game.ReplicatedStorage.Remotes.CommF_:InvokeServer("Wenlocktoad","1")
+local b = game.ReplicatedStorage.Remotes.CommF_:InvokeServer("Alchemist","1")
+
+if game.Players.LocalPlayer.Character:FindFirstChild("RaceTransformed") then
+return game:GetService("Players").LocalPlayer.Data.Race.Value.." V4"
+end
+
+if a == -2 then
+return game:GetService("Players").LocalPlayer.Data.Race.Value.." V3"
+end
+
+if b == -2 then
+return game:GetService("Players").LocalPlayer.Data.Race.Value.." V2"
+end
+
+return game:GetService("Players").LocalPlayer.Data.Race.Value.." V1"
+end
+
+local CRace = Tabs.Race:AddParagraph({
+    Title = "Your Race",
     Content = "..."
 })
 
 spawn(function()
-    while task.wait() do
+    while wait() do
         pcall(function()
-            Moon1:SetDesc("Moon: "..CheckMoon())
+            CRace:SetDesc("Race: "..CheckRace())
         end)
     end
 end)
@@ -7726,7 +7644,7 @@ local TeleportIsland = Tabs.Teleport:AddToggle("TeleportIslan", {Title = "Telepo
 TeleportIsland:OnChanged(function(Value)
     _G.TeleportIsland = Value
     if _G.TeleportIsland == true then
-        repeat task.wait()
+        repeat wait()
             if _G.SelectIsland == "WindMill" then
                 topos(CFrame.new(979.79895019531, 16.516613006592, 1429.0466308594))
             elseif _G.SelectIsland == "Marine" then
@@ -7868,6 +7786,79 @@ spawn(function()
     end
 end)
 
+function MoonTextureId()
+    if World1 then
+        return game:GetService("Lighting").FantasySky.MoonTextureId
+    elseif World2 then
+        return game:GetService("Lighting").FantasySky.MoonTextureId
+    elseif World3 then
+        return game:GetService("Lighting").Sky.MoonTextureId
+    end
+end
+function CheckMoon()
+    moon8 = "http://www.roblox.com/asset/?id=9709150401"
+    moon7 = "http://www.roblox.com/asset/?id=9709150086"
+    moon6 = "http://www.roblox.com/asset/?id=9709149680"
+    moon5 = "http://www.roblox.com/asset/?id=9709149431"
+    moon4 = "http://www.roblox.com/asset/?id=9709149052"
+    moon3 = "http://www.roblox.com/asset/?id=9709143733"
+    moon2 = "http://www.roblox.com/asset/?id=9709139597"
+    moon1 = "http://www.roblox.com/asset/?id=9709135895"
+    moonreal = MoonTextureId()
+    cofullmoonkothangbeo = "Bad Moon"
+    if moonreal == moon5 or moonreal == moon4 then
+        if moonreal == moon5 then
+            cofullmoonkothangbeo = "Full Moon"
+        elseif moonreal == moon4 then
+            cofullmoonkothangbeo = "Next Night"
+        end
+    end
+    return cofullmoonkothangbeo
+end
+function function7()
+    GameTime = "Error"
+    local c = game.Lighting
+    local ao = c.ClockTime
+    if ao >= 18 or ao < 5 then
+        GameTime = "Night"
+    else
+        GameTime = "Day"
+    end
+    return GameTime
+end
+function function6()
+    return math.floor(game.Lighting.ClockTime)
+end
+function getServerTime()
+    RealTime = tostring(math.floor(game.Lighting.ClockTime * 100) / 100)
+    RealTime = tostring(game.Lighting.ClockTime)
+    RealTimeTable = RealTime:split(".")
+    Minute, Second = RealTimeTable[1], tonumber(0 + tonumber(RealTimeTable[2] / 100)) * 60
+    return Minute, Second
+end
+function function8()
+    local c = game.Lighting
+    local ao = c.ClockTime
+    if CheckMoon() == "Full Moon" and ao <= 5 then
+        return tostring(function6()) .. " ( Will End Moon In " .. math.floor(5 - ao) .. " Minutes )"
+    elseif CheckMoon() == "Full Moon" and (ao > 5 and ao < 12) then
+        return tostring(function6()) .. " ( Fake Moon )"
+    elseif CheckMoon() == "Full Moon" and (ao > 12 and ao < 18) then
+        return tostring(function6()) .. " ( Will Full Moon In " .. math.floor(18 - ao) .. " Minutes )"
+    elseif CheckMoon() == "Full Moon" and (ao > 18 and ao <= 24) then
+        return tostring(function6()) .. " ( Will End Moon In " .. math.floor(24 + 6 - ao) .. " Minutes )"
+    end
+    if CheckMoon() == "Next Night" and ao < 12 then
+        return tostring(function6()) .. " ( Will Full Moon In " .. math.floor(18 - ao) .. " Minutes )"
+    elseif CheckMoon() == "Next Night" and ao > 12 then
+        return tostring(function6()) .. " ( Will Full Moon In " .. math.floor(18 + 12 - ao) .. " Minutes )"
+    end
+    return tostring(function6())
+end
+function FullMoobCheck()
+ return function8()
+end
+
 local Moon = Tabs.StatusServer:AddParagraph({
     Title = "Full Moon Status",
     Content = "..."
@@ -7876,7 +7867,7 @@ local Moon = Tabs.StatusServer:AddParagraph({
 spawn(function()
     while task.wait() do
         pcall(function()
-            Moon:SetDesc("Moon: "..CheckMoon())
+            Moon:SetDesc(CheckMoon())
         end)
     end
 end)
