e56172bf7f6743f1946b6c7a684f233c971395f2
Shisvwns <165137013+Shisvwns@users.noreply.github.com>
2024-04-13 07:55:38 +0700

    Update Cjsbshish.lua

--- Cjsbshish.lua
+++ Cjsbshish.lua
@@ -6762,7 +6762,7 @@ local Slplayer = Player:AddDropdown({
 Player:AddButton({
     Name = "Refresh Player",
     Callback = function()
-        Slplayer:Refresh(Playerslist,true)
+        table.clear(Playerslist)
         for i,v in pairs(game:GetService("Players"):GetChildren()) do
             table.insert(Playerslist,v.Name)
         end
