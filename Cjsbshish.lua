4961a19bf0db18b63e90dd61c6eb52380d84ad3b
Shisvwns <165137013+Shisvwns@users.noreply.github.com>
2024-04-28 09:13:08 +0700

    Update Cjsbshish.lua

--- Cjsbshish.lua
+++ Cjsbshish.lua
@@ -2167,32 +2167,39 @@ function BTP(p)
     end)
 end
 
+function TelePPlayer(P)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = P
+end
+
 function topos(Pos)
+    function WaitHRP(Player)
+        if not Player then return end
+        return Player.Character:WaitForChild("HumanoidRootPart", 9)
+    end
     local LocalPlayer = game.Players.LocalPlayer
     local Character = LocalPlayer.Character
-    local Humanoid = Character:WaitForChild("Humanoid")
-    local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
-    if Humanoid.Sit = true then
-        Humanoid.Sit = false
-    end    
-    local Distance = (Pos.Position - HumanoidRootPart.Position).Magnitude
-    local PartTele = Instance.new("Part", Character)
-    PartTele.Size = Vector3.new(0, 0, 0)
-    PartTele.Name = "PartTele"
-    PartTele.Anchored = true
-    PartTele.Transparency = 1
-    PartTele.CanCollide = false
-    PartTele.CFrame = HumanoidRootPart.CFrame
-    local Tween = game:GetService("TweenService"):Create(PartTele, TweenInfo.new(Distance / 350, Enum.EasingStyle.Linear), {CFrame = Pos})
+    local Distance = (Pos.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
+    if game.Players.LocalPlayer.Character.Humanoid.Sit == true then
+        game.Players.LocalPlayer.Character.Humanoid.Sit = false
+    end
+    if not Character:FindFirstChild("PartTele") then
+        local PartTele = Instance.new("Part", Character)
+        PartTele.Size = Vector3.new(0, 0, 0)
+        PartTele.Name = "PartTele"
+        PartTele.Anchored = true
+        PartTele.Transparency = 1
+        PartTele.CanCollide = false
+        PartTele.CFrame = WaitHRP(LocalPlayer).CFrame
+        PartTele:GetPropertyChangedSignal("CFrame"):Connect(function()
+            task.wait()
+            WaitHRP(LocalPlayer).CFrame = PartTele.CFrame
+        end)
+    end
+    local Tween = game:GetService("TweenService"):Create(Character.PartTele, TweenInfo.new(Distance / 350, Enum.EasingStyle.Linear), {CFrame = Pos})
     Tween:Play()
     if _G.StopTween == true then
         Tween:Cancel()
-        HumanoidRootPart.CFrame = Pos
     end
-end
-
-function TelePPlayer(P)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = P
 end
 
 Type = 1
