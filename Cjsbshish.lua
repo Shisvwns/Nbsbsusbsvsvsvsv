efe06c2f914574149baf96bdcc9f8dac4cbc7d0c
Shisvwns <165137013+Shisvwns@users.noreply.github.com>
2024-05-13 08:43:14 +0700

    Update Cjsbshish.lua

--- Cjsbshish.lua
+++ Cjsbshish.lua
@@ -2274,7 +2274,7 @@ Farm:AddToggle({
 })
 
 spawn(function()
-    while task.wait() do
+    while wait() do
         pcall(function()
             if _G.BringMonster then
                 CheckQuest()
@@ -2392,8 +2392,8 @@ spawn(function()
                         end
                     end
                     if _G.AutoDoughtBoss and MagnetDought then
-                        if (v.Name == "Cookie Crafter" or v.Name == "Cake Guard" or v.Name == "Baking Staff" or v.Name == "Head Baker") and v:FindFirstChild("HumanoidRootPart") and v:FindFirstChild("Humanoid") and v.Humanoid.Health > 0 and GetDistance(v.HumanoidRootPart.Position) <= 300 then
-                            v.HumanoidRootPart.Size = Vector3.new(50,50,50)
+                        if (v.Name == "Cookie Crafter" or v.Name == "Cake Guard" or v.Name == "Baking Staff" or v.Name == "Head Baker") and (v.HumanoidRootPart.Position - PosMonDoughtOpenDoor.Position).Magnitude <= _G.BringMode and v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health > 0 then
+                            v.HumanoidRootPart.Size = Vector3.new(1,1,1)
                             v.Humanoid:ChangeState(14)
                             v.HumanoidRootPart.CanCollide = false
                             v.Head.CanCollide = false
@@ -2435,7 +2435,7 @@ spawn(function()
                     v.Humanoid.Animator:Destroy()
                 end
                 v.Humanoid:ChangeState(14)
-                sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius",  math.huge)
+                sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", math.huge)
             end
         end
     end
