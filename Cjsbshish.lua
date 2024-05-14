4aced602c62ba2e4059bab0a7f90d377237d3847
Shisvwns <165137013+Shisvwns@users.noreply.github.com>
2024-05-14 21:08:14 +0700

    Update Cjsbshish.lua

--- Cjsbshish.lua
+++ Cjsbshish.lua
@@ -2338,7 +2338,6 @@ spawn(function()
                         if v.Name == "Shanda" and (v.HumanoidRootPart.Position - PosMon.Position).Magnitude <= 350 and v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health > 0 then
                             v.HumanoidRootPart.CFrame = PosMon
                             v.HumanoidRootPart.Size = Vector3.new(50,50,50)
-                            v.Humanoid:ChangeState(14)
                             v.HumanoidRootPart.CanCollide = false
                             v.Head.CanCollide = false
                             v.Humanoid.JumpPower = 0
@@ -2347,7 +2346,7 @@ spawn(function()
                                 v.Humanoid.Animator:Destroy()
                             end
                             v.Humanoid:ChangeState(14)
-                            sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", math.huge)
+                            sethiddenproperty(game:GetService("Players").LocalPlayer, "SimulationRadius", math.huge)
                         end
                     end
                 end
