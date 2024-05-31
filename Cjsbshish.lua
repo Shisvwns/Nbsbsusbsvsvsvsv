d335687ab312f7804bf81b1f884a5b725b826d2a
Shisvwns <165137013+Shisvwns@users.noreply.github.com>
2024-06-01 06:49:17 +0700

    Update Cjsbshish.lua

--- Cjsbshish.lua
+++ Cjsbshish.lua
@@ -3755,7 +3755,6 @@ spawn(function()
                 if game:GetService("Workspace").Enemies:FindFirstChild("Ship Deckhand") or game:GetService("Workspace").Enemies:FindFirstChild("Ship Engineer") or game:GetService("Workspace").Enemies:FindFirstChild("Ship Steward") or game:GetService("Workspace").Enemies:FindFirstChild("Ship Officer") then
                     for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
                         if v.Name == "Ship Deckhand" or v.Name == "Ship Engineer" or v.Name == "Ship Steward" or v.Name == "Ship Officer" then
-                            if string.find(v.Name,"Ship") then
                                 repeat task.wait()
                                     EquipWeapon(_G.SelectWeapon)
                                     topos(v.HumanoidRootPart.CFrame * Pos)
@@ -3763,7 +3762,6 @@ spawn(function()
                                     StartEctoplasmMagnet = true
                                 until not _G.AutoMaterial or not v.Parent or v.Humanoid.Health <= 0
                                 StartEctoplasmMagnet = false
-                            end
                         end
                     end
                 else
