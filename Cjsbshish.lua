63028af1a3097f6dd850fb7a420b8d6c5e4fd3b8
Shisvwns <165137013+Shisvwns@users.noreply.github.com>
2024-08-17 22:08:29 +0700

    Update Cjsbshish.lua

--- Cjsbshish.lua
+++ Cjsbshish.lua
@@ -1536,7 +1536,7 @@ function Tween(Pos)
     Distance = (Pos.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
     if Distance <= 250 then
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Pos
-    else
+    end
     TweenPos = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(Distance/_G.TweenSpeed, Enum.EasingStyle.Linear),{CFrame = Pos})
     TweenPos:Play()
 end
