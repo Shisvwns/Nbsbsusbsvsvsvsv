19b3effce2029a00eb459d6245d92532b0947e71
Shisvwns <165137013+Shisvwns@users.noreply.github.com>
2024-11-09 16:54:58 +0700

    Update Testui.lua

--- Testui.lua
+++ Testui.lua
@@ -1637,9 +1637,9 @@ function OrionLib:MakeWindow(WindowConfig)
 			}), {
 				AddThemeObject(SetProps(MakeElement("Label", SectionConfig.Name, 14), {
 					Size = UDim2.new(1, -12, 0, 16),
-					Position = UDim2.new(0.5, 0, 0, 3),
+					Position = UDim2.new(0, 0, 0, 3),
 					Font = Enum.Font.FredokaOne,
-					TextYAlignment = Enum.TextYAlignment.Center
+					TextYAlignment = Enum.TextXAlignment.Center,
 				}), "TextDark"),
 				SetChildren(SetProps(MakeElement("TFrame"), {
 					AnchorPoint = Vector2.new(0, 0),
